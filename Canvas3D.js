3ca58e8619738f373235e1fff8a3735f53bdd723
Darío Alejandro LONCÓN <lonky6039@gmail.com>
2026-04-09 15:12:15 -0300

    Update Canvas3D.js

--- Canvas3D.js
+++ Canvas3D.js
@@ -12,3 +12,30 @@ function crearColumna(x, y, scene) {
 }
 <Canvas3D tool={tool} material={material} />
 export default function Canvas3D({ tool, material }) {
+const loader = new THREE.TextureLoader();
+
+function obtenerMaterial(tipo) {
+  let textura;
+
+  switch (tipo) {
+    case "ladrillo_macizo":
+      textura = loader.load("/textures/ladrillo.jpg");
+      break;
+    case "ladrillo_ceramico":
+      textura = loader.load("/textures/ceramico.jpg");
+      break;
+    case "bloque":
+    case "hormigon":
+      textura = loader.load("/textures/hormigon.jpg");
+      break;
+  }
+
+  textura.wrapS = textura.wrapT = THREE.RepeatWrapping;
+  textura.repeat.set(2, 2);
+
+  return new THREE.MeshStandardMaterial({
+    map: textura,
+    roughness: 0.8,
+    metalness: 0.1
+  });
+}
