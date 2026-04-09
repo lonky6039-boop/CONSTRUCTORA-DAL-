2a8edeff46adf3884068bde5b7580899a19e8dec
Darío Alejandro LONCÓN <lonky6039@gmail.com>
2026-04-09 13:49:19 -0300

    Create Normativas.js

---
+++ Normativas.js
@@ -0,0 +1,17 @@
+export const normas = {
+  argentina: {
+    nombre: "CIRSOC",
+    tensionMax: 140,
+    deformacionMax: 0.02
+  },
+  chile: {
+    nombre: "NCh",
+    tensionMax: 130,
+    deformacionMax: 0.025
+  },
+  mexico: {
+    nombre: "NTC",
+    tensionMax: 150,
+    deformacionMax: 0.02
+  }
+};
