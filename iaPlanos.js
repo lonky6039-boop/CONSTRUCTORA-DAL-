071d97ead541f2532cc522be81f88df3232765c7
Darío Alejandro LONCÓN <lonky6039@gmail.com>
2026-04-09 15:05:24 -0300

    Update iaPlanos.js

--- iaPlanos.js
+++ iaPlanos.js
@@ -7,3 +7,5 @@ export function generarPlanoIA(input) {
     { tipo: "muro", p1: { x: 0, y: 5 }, p2: { x: 0, y: 0 } },
   ];
 }
+const grid = new THREE.GridHelper(50, 50, 0x1E3A8A, 0x1E3A8A);
+scene.add(grid);
