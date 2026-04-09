d78bd5d3a34f60ad8f826cee7cad59fce7085944
Darío Alejandro LONCÓN <lonky6039@gmail.com>
2026-04-09 13:19:23 -0300

    Add function to generate basic wall plan

---
+++ iaPlanos.js
@@ -0,0 +1,9 @@
+export function generarPlanoIA(input) {
+  // Interpretación básica (expandible con IA real)
+  return [
+    { tipo: "muro", p1: { x: 0, y: 0 }, p2: { x: 7, y: 0 } },
+    { tipo: "muro", p1: { x: 7, y: 0 }, p2: { x: 7, y: 5 } },
+    { tipo: "muro", p1: { x: 7, y: 5 }, p2: { x: 0, y: 5 } },
+    { tipo: "muro", p1: { x: 0, y: 5 }, p2: { x: 0, y: 0 } },
+  ];
+}
