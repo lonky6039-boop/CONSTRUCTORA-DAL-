77927cca5347db9c78f83f3c50a1d7d305945b73
Darío Alejandro LONCÓN <lonky6039@gmail.com>
2026-04-09 12:39:13 -0300

    Add Materiales.js with construction material data

---
+++ Materiales.js
@@ -0,0 +1,20 @@
+export const materiales = {
+  muro: {
+    espesor: 0.15,
+    altura: 3,
+    tipo: "ladrillo hueco",
+  },
+  columna: {
+    ancho: 0.2,
+    profundidad: 0.2,
+    hierro: "4Ø10 + estribos Ø6 c/20cm",
+  },
+  viga: {
+    ancho: 0.2,
+    alto: 0.3,
+  },
+  cimiento: {
+    ancho: 0.4,
+    profundidad: 0.3,
+  }
+};
