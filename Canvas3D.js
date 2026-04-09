dbe3659b34a6b4f24a25351c54cd41384e1a7047
Darío Alejandro LONCÓN <lonky6039@gmail.com>
2026-04-09 14:58:17 -0300

    Update Canvas3D.js

--- Canvas3D.js
+++ Canvas3D.js
@@ -1,6 +1,9 @@
 function crearColumna(x, y, scene) {
   const geo = new THREE.BoxGeometry(0.2, 0.2, 3);
-  const mat = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+
+  const mat = new THREE.MeshStandardMaterial({
+    color: 0x374151
+  });
 
   const col = new THREE.Mesh(geo, mat);
   col.position.set(x, y, 1.5);
